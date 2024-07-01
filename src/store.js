99d55190a26f1dbc91c92aa49529341e667f55be
ghaith <mlika.gaith@gmail.com>
2024-07-01 16:12:07 +0100

    Add: game sounds effects

--- src/store.js
+++ src/store.js
@@ -1,4 +1,17 @@
 import { create } from "zustand";
+import startSound from "/src/assets/sounds/start.wav";
+import loseSound from "/src/assets/sounds/lose.wav";
+import ballDropSound from "/src/assets/sounds/ball-drop.wav";
+import pingSound from "/src/assets/sounds/ping.mp3";
+import backgroundSound from "/src/assets/sounds/background.mp3";
+
+const start = new Audio(startSound);
+const lose = new Audio(loseSound);
+const ballDrop = new Audio(ballDropSound);
+const ping = new Audio(pingSound);
+const background = new Audio(backgroundSound);
+background.loop = true;
+background.volume = 0.8;
 
 const useGameStore = create((set) => ({
   points: 0,
@@ -24,23 +37,33 @@ const useGameStore = create((set) => ({
     { right: false, long: true, y: 4.5, speed: 0.135, color: "#00ffff" },
   ],
   start: () => {
+    start.play((start.currentTime = 0));
     set({ startup: false, hearts: 3, totalScore: 0 });
+    background.play((background.currentTime = 0));
     document.body.style.cursor = "none";
   },
   reset: () => {
-    set((state) => ({
-      ...state,
-      points: 0,
-      hearts: state.hearts > 0 ? state.hearts - 1 : 0,
-      restart: state.hearts === 0 ? false : true,
-      startup: state.hearts === 0 && true,
-      totalScore: state.hearts === 0 ? 0 : state.totalScore + state.points,
-    }));
+    ballDrop.play((ballDrop.currentTime = 0));
+    set((state) => {
+      if (state.hearts === 1) {
+        lose.play((lose.currentTime = 0));
+        background.pause();
+      }
+      return {
+        ...state,
+        points: 0,
+        hearts: state.hearts > 0 ? state.hearts - 1 : 0,
+        restart: state.hearts === 0 ? false : true,
+        startup: state.hearts === 0 && true,
+        totalScore: state.hearts === 0 ? 0 : state.totalScore + state.points,
+      };
+    });
     setTimeout(() => set((state) => ({ ...state, restart: false })), 10);
   },
   contact: (e) => {
     if (e.contact.impactVelocity > 4)
       set((state) => ({ points: state.points + 1 }));
+    ping.play((ping.currentTime = 0));
   },
 }));
 
